4b2f5bbdb4629c0e2330a64505c70a1c2e96c7d9
先小山　将虎 <sakikoyama@a0001325.local>
2017-03-14 12:31:26 +0900

    微修正

--- numoffrieds.py
+++ numoffrieds.py
@@ -1,6 +1,13 @@
 # -*- coding:utf-8 -*-
+# return 2
+friends0 = [
+        'NYY',
+        'YNY',
+        'YYN'
+    ]
 
-friends = [
+# return 4
+friends1 = [
         'NYNNN',
         'YNYNN',
         'NYNYN',
@@ -8,14 +15,43 @@ friends = [
         'NNNYN'
     ]
 
+# return 8
+friends2 = [
+        'NNNNYNNNNN',
+        'NNNNYNYYNN',
+        'NNNYYYNNNN',
+        'NNYNNNNNNN',
+        'YYYNNNNNNY',
+        'NNYNNNNNYN',
+        'NYNNNNNYNN',
+        'NYNNNNYNNN',
+        'NNNNNYNNNN',
+        'NNNNYNNNNN'
+    ]
+
+# return 6
+friends3 = [
+        'NNNNNNNNNNNNNNY',
+        'NNNNNNNNNNNNNNN',
+        'NNNNNNNYNNNNNNN',
+        'NNNNNNNYNNNNNNY',
+        'NNNNNNNNNNNNNNY',
+        'NNNNNNNNYNNNNNN',
+        'NNNNNNNNNNNNNNN',
+        'NNYYNNNNNNNNNNN',
+        'NNNNNYNNNNNYNNN',
+        'NNNNNNNNNNNNNNY',
+        'NNNNNNNNNNNNNNN',
+        'NNNNNNNNYNNNNNN',
+        'NNNNNNNNNNNNNNN',
+        'NNNNNNNNNNNNNNN',
+        'YNNYYNNNNYNNNNN'
+    ]
 
 class Human(object):
     def __init__(self, index):
-        self.index = index,
+        self.index = index
         self.friends = set()
-
-    def get_index(self):
-        return self.index[0]
 
     def get_friends_num(self):
         return len(self.friends)
@@ -25,28 +61,46 @@ def recursion_depth(friends, maxtimes=2):
     def recusion(human, items, times=0):
         times += 1
         for i, h in enumerate(items):
-            if h == 'Y' and human.get_index() != i and times <= maxtimes:
+            if h == 'Y' and human.index != i and times <= maxtimes:
                 human.friends.add(i)
                 recusion(human, friends[i], times)
         return human
 
     for index, items in enumerate(friends):
-        yield recusion(Human(index), items).get_friends_num()
+        yield recusion(Human(index), items).friends
 
 
-def recursion_width():
+def roop_width(friends, maxtimes=2):
+    def roop(human, items):
+        temp = []
+        for i, h in enumerate(items):
+            if h == 'Y' and human.index != i:
+                temp.append(i)
+        return temp
 
-
-def roop_depth():
-    pass
-
-
-def roop_width():
-    pass
+    tmp = []
+    for index, items in enumerate(friends):
+        human = Human(index)
+        for i in roop(human, items):
+            human.friends.add(i)
+        tmp.append(human)
+    times = 0
+    while True:
+        times += 1
+        if times >= maxtimes:
+            break
+        for human in tmp:
+            temp = []
+            for i in human.friends:
+                temp.extend(roop(human, friends[i]))
+            for i in temp:
+                human.friends.add(i)
+    return [x.friends for x in tmp]
 
 
 def main():
-    print max(list(recursion_depth(friends)))
+    print list(recursion_depth(friends2))
+    print roop_width(friends2)
 
 
 if __name__ == '__main__':
